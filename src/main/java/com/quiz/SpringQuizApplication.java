d314b18433d8c2f5d153f58410946eaa99dd936d
DESKTOP-44HIMKR\admin <test@test.com>
2023-12-15 18:45:37 +0900

    quiz 로그 적용하기

--- src/main/java/com/quiz/SpringQuizApplication.java
+++ src/main/java/com/quiz/SpringQuizApplication.java
@@ -12,22 +12,12 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 
-@MapperScan(basePackages = "com.quiz.*")
 @SpringBootApplication
 public class SpringQuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringQuizApplication.class, args);
 	}
-	@Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-
-        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
-        sessionFactory.setMapperLocations(res);
-
-        return sessionFactory.getObject();
-    }
+	
 
 }
